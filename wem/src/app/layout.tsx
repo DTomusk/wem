f8a7af98aa79d8789ef06553dd02455dbdf9a4c7
Dave <david.tomusk@gmail.com>
2025-08-15 19:22:26 +0100

    Set up i18

--- wem/src/app/layout.tsx
+++ wem/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientLayout from "../components/ClientLayout";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../../i18";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <I18nextProvider i18n={i18n}>
           <ClientLayout>
             {children}
           </ClientLayout>
+        </I18nextProvider>
       </body>
     </html>
   );
