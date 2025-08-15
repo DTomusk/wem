845213df9a9fbd56b6a75d02b664a85c7e74cecc
Dave <david.tomusk@gmail.com>
2025-08-15 19:37:10 +0100

    Update navbar naming

--- wem/src/app/layout.tsx
+++ wem/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Container } from "@mui/material";
-import Nav from "../components/Nav";
+import NavBar from "../components/NavBar";
 import ThemeRegistry from "../components/ThemeRegistry";
 
 const geistSans = Geist({
@@ -31,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeRegistry defaultMode="light">
-          <Nav />
+          <NavBar />
           <Container maxWidth="md" sx={{
             display: "flex",
             flexDirection: "column",
--- wem/src/components/NavBar.tsx
+++ wem/src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import ThemeToggle from "./ThemeToggle";
 
-export default function Nav() {
+export default function NavBar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
